a799093d268e3823f583b2e30d22ed26035402ca
sohelrana76 <msrana3876@gmail.com>
2021-03-20 18:37:17 +0600

    final done

---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    const {img, name, seller, price, stock} = props.product;
+    
+    return (
+        <div className="product">
+            <div>
+                <img src={img} alt=""/>
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br/>
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p><small>Only {stock} left in stock - order soon</small></p>
+                <button className="main-button" 
+                onClick={() => props.handleAddProduct(props.product)}
+                >Add to cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
